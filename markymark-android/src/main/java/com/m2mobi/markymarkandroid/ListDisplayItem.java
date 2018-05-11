a81101745562f6ff3a34e53c229a63a06c3969e7
niels.masdorp <n.masdorp@m2mobi.com>
2018-05-11 11:45:03 +0200

    [Bug] Fixed bug where bullets were placed too low

--- markymark-android/src/main/java/com/m2mobi/markymarkandroid/ListDisplayItem.java
+++ markymark-android/src/main/java/com/m2mobi/markymarkandroid/ListDisplayItem.java
@@ -6,14 +6,15 @@ package com.m2mobi.markymarkandroid;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.ColorFilter;
+import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.DimenRes;
 import android.support.annotation.DrawableRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
+import android.text.style.LeadingMarginSpan;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TableLayout.LayoutParams;
@@ -25,89 +26,58 @@ import com.m2mobi.markymarkcommon.markdownitems.ListItem;
 import com.m2mobi.markymarkcommon.markdownitems.MarkdownList;
 
 import java.util.List;
-import java.util.Locale;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 /**
- * Converts an {@link MarkdownList} to a {@link View}.
+ * Creates a view containing list items created from a {@link MarkdownList}
+ * <p>
+ * User: M2mobi
+ * Date 04/05/2016
+ * Time 11:31
  */
 public class ListDisplayItem implements DisplayItem<View, MarkdownList, Spanned> {
 
 	private Context mContext;
 
-	private final int mIndentationSpacingPixels, mIndicatorSpacingPixels;
+	private final int mListMargin;
 
 	@DrawableRes
 	private final int[] mBulletDrawables;
 
-	public ListDisplayItem(final Context pContext, @DimenRes final int pIndentationSpacing,
-			@DimenRes final int pBulletSpacing, @DrawableRes final int[] pBulletDrawables) {
+	public ListDisplayItem(final Context pContext, @DimenRes final int pListMargin, @DrawableRes final int[] pBulletDrawables) {
 		mContext = pContext;
-		mIndicatorSpacingPixels = mContext.getResources().getDimensionPixelSize(pBulletSpacing);
-		mIndentationSpacingPixels = mContext.getResources().getDimensionPixelSize(pIndentationSpacing);
+		mListMargin = mContext.getResources().getDimensionPixelSize(pListMargin);
 		mBulletDrawables = pBulletDrawables;
 	}
 
 	@Override
-	public View create(final MarkdownList pMarkdownItem, final InlineConverter<Spanned> pInlineConverter) {
-		return createLayout(pMarkdownItem, pInlineConverter);
+	public View create(final MarkdownList pMarkDownItem, final InlineConverter<Spanned> pInlineConverter) {
+		return createLayout(pMarkDownItem, pInlineConverter);
 	}
 
-	private LinearLayout createLayout(final MarkdownList pMarkdownItem, final InlineConverter<Spanned> pInlineConverter) {
+	private LinearLayout createLayout(final MarkdownList pMarkDownItem, final InlineConverter<Spanned> pInlineConverter) {
 		LinearLayout layout = new LinearLayout(mContext);
 		layout.setLayoutParams(new LayoutParams(MATCH_PARENT, WRAP_CONTENT));
 		layout.setOrientation(LinearLayout.VERTICAL);
 
-		final List<ListItem> items = pMarkdownItem.getListItems();
+		final List<ListItem> items = pMarkDownItem.getListItems();
 		for (int i = 0; i < items.size(); i++) {
 			final ListItem item = items.get(i);
-			final View view = getTextView(pMarkdownItem, pInlineConverter.convert(item.getText()), i);
+			final View view = getTextView(pMarkDownItem, pInlineConverter.convert(item.getText()), i);
 			layout.addView(view);
 
 			if (item.hasChild()) {
 				for (MarkdownList list : item.getChild()) {
 					final LinearLayout nestedLayout = createLayout(list, pInlineConverter);
-					LayoutParams params = new LayoutParams(
-							LayoutParams.WRAP_CONTENT,
-							LayoutParams.WRAP_CONTENT
-					);
-					params.setMargins(mIndentationSpacingPixels, 0, 0, 0);
-					nestedLayout.setLayoutParams(params);
+					nestedLayout.setPadding(mListMargin, 0, 0, 0);
 					layout.addView(nestedLayout);
 				}
 			}
 		}
-		return layout;
-	}
 
-	/**
-	 * Creates a TextView that represents a list item
-	 *
-	 * @param pMarkdownList
-	 * 		MarkdownList used to determine the kind of list
-	 * @param pText
-	 * 		Content of the list item
-	 * @param pIndex
-	 * 		Index of the item in the list
-	 * @return Returns a TextView
-	 */
-	private TextView getTextView(final MarkdownList pMarkdownList, final CharSequence pText, final int pIndex) {
-		final TextView textView = new TextView(mContext, null, R.attr.MarkdownListStyle);
-		textView.setMovementMethod(LinkMovementMethod.getInstance());
-		if (pMarkdownList.isOrdered()) {
-			textView.setText(String.format(Locale.getDefault(), "%d. %s", pIndex + 1, pText));
-		} else {
-			Drawable bullet = mContext.getResources().getDrawable(getDrawable(pMarkdownList.getNestedLevel()));
-			TopCompoundDrawable gravityDrawable = new TopCompoundDrawable(bullet, textView.getLineHeight());
-			bullet.setBounds(0, 0, bullet.getIntrinsicWidth(), bullet.getIntrinsicHeight());
-			gravityDrawable.setBounds(0, 0, bullet.getIntrinsicWidth(), bullet.getIntrinsicHeight());
-			textView.setCompoundDrawables(gravityDrawable, null, null, null);
-			textView.setCompoundDrawablePadding(mIndicatorSpacingPixels);
-			textView.setText(pText);
-		}
-		return textView;
+		return layout;
 	}
 
 	/**
@@ -125,54 +95,112 @@ public class ListDisplayItem implements DisplayItem<View, MarkdownList, Spanned>
 	}
 
 	/**
-	 * Drawable that aligns vertically on the first line of the TextView
+	 * Creates a TextView that represents a list item
+	 *
+	 * @param pMarkDownList
+	 * 		MarkDownList used to determine the kind of list
+	 * @param pText
+	 * 		Content of the list item
+	 * @param pIndex
+	 * 		Index of the item in the list
+	 * @return Returns a TextView
 	 */
-	public class TopCompoundDrawable extends Drawable {
+	private TextView getTextView(final MarkdownList pMarkDownList, final CharSequence pText, final int pIndex) {
+		final TextView textView = new TextView(mContext, null, R.attr.MarkdownListStyle);
+		textView.setMovementMethod(LinkMovementMethod.getInstance());
 
+		if (pMarkDownList.isOrdered()) {
+			SpannableString string = new SpannableString(pText);
+			string.setSpan(new NumberSpan(mListMargin, pIndex + 1), 0, string.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+			textView.setText(string);
+		} else {
+			final Drawable drawable = mContext.getResources().getDrawable(getDrawable(pMarkDownList.getNestedLevel()));
+			SpannableString string = new SpannableString(pText);
+			string.setSpan(new BulletSpan(mListMargin, drawable), 0, string.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+			textView.setText(string);
+		}
+
+		return textView;
+	}
+
+	/**
+	 * Span that adds a number in front of the first line of a list item
+	 */
+	private class NumberSpan implements LeadingMarginSpan.LeadingMarginSpan2 {
+
+		/** Width of the margin */
+		private final int mGapWidth;
+
+		/** Number of the list item */
+		private final int mIndex;
+
+		public NumberSpan(int pGapWidth, int pIndex) {
+			mGapWidth = pGapWidth;
+			mIndex = pIndex;
+		}
+
+		@Override
+		public void drawLeadingMargin(Canvas pCanvas, Paint pPaint, int pX, int pDir, int pTop, int pBaseline, int pBottom,
+				CharSequence pText, int pStart, int pEnd, boolean pFirst, Layout pLayout) {
+			if (pFirst) {
+				final String number = mIndex + ". ";
+				final float v = pPaint.measureText(number, 0, number.length());
+				pCanvas.drawText(number, mGapWidth - v, pBaseline, pPaint);
+			}
+		}
+
+		@Override
+		public int getLeadingMargin(final boolean first) {
+			return mGapWidth;
+		}
+
+		@Override
+		public int getLeadingMarginLineCount() {
+			// Only add to first line
+			return 1;
+		}
+	}
+
+	/**
+	 * Span that adds a bullet in front of the first line of a list item
+	 */
+	private class BulletSpan implements LeadingMarginSpan.LeadingMarginSpan2 {
+
+		/** Width of the margin */
+		private final int mGapWidth;
+
+		/** Drawable that will be drawn in front of the first line */
 		private final Drawable mDrawable;
 
-		private final int mTextLineHeight;
-
-		public TopCompoundDrawable(Drawable drawable, int textLineHeight) {
-			mDrawable = drawable;
-			mTextLineHeight = textLineHeight;
+		public BulletSpan(int pGapWidth, final Drawable pDrawable) {
+			mGapWidth = pGapWidth;
+			mDrawable = pDrawable;
 		}
 
 		@Override
-		public int getIntrinsicWidth() {
-			return mDrawable.getIntrinsicWidth();
+		public int getLeadingMargin(final boolean first) {
+			return mGapWidth;
 		}
 
 		@Override
-		public int getIntrinsicHeight() {
-			return mDrawable.getIntrinsicHeight();
+		public void drawLeadingMargin(Canvas pCanvas, Paint pPaint, int pX, int pDir, int pTop, int pBaseline, int pBottom,
+				CharSequence pText, int pStart, int pEnd, boolean pFirst, Layout pLayout) {
+			if (!pFirst) {
+				return;
+			}
+			int drawableHeight = mDrawable.getMinimumHeight();
+			int drawableWidth = mDrawable.getMinimumWidth();
+			int y = (pBaseline / 2);
+			int x = (mGapWidth / 2);
+
+			mDrawable.setBounds(x, y, x + drawableWidth, y + drawableHeight);
+			mDrawable.draw(pCanvas);
 		}
 
 		@Override
-		public void draw(@NonNull Canvas canvas) {
-			int halfCanvas = canvas.getHeight() / 2;
-			int halfLineHeight = mTextLineHeight / 2;
-
-			// align to top
-			canvas.save();
-			canvas.translate(0, -halfCanvas + halfLineHeight);
-			mDrawable.draw(canvas);
-			canvas.restore();
-		}
-
-		@Override
-		public void setAlpha(final int alpha) {
-			mDrawable.setAlpha(alpha);
-		}
-
-		@Override
-		public void setColorFilter(@Nullable final ColorFilter colorFilter) {
-			mDrawable.setColorFilter(colorFilter);
-		}
-
-		@Override
-		public int getOpacity() {
-			return mDrawable.getOpacity();
+		public int getLeadingMarginLineCount() {
+			// Only add to first line
+			return 1;
 		}
 	}
 }
--- markymark-android/src/main/java/com/m2mobi/markymarkandroid/MarkyMarkAndroid.java
+++ markymark-android/src/main/java/com/m2mobi/markymarkandroid/MarkyMarkAndroid.java
@@ -31,80 +31,87 @@ import java.util.List;
  */
 public class MarkyMarkAndroid {
 
-    /**
-     * Creates a MarkyMark instance that converts the rules provided by the flavor into MarkyMarkAndroid Views
-     *
-     * @param pContext    Context used to create Views
-     * @param pFlavor     Flavor that provides the markdown parsing rules
-     * @param imageLoader The ImageLoader that should be used to load images
-     * @return MarkMark instance that renders MarkyMarkAndroid Views from Markdown
-     */
-    public static MarkyMark<View> getMarkyMark(final Context pContext, final Flavor pFlavor, @NonNull final ImageLoader imageLoader) {
-        return getMarkyMark(pContext, pFlavor, null, null, imageLoader);
-    }
+	/**
+	 * Creates a MarkyMark instance that converts the rules provided by the flavor into MarkyMarkAndroid Views
+	 *
+	 * @param pContext
+	 * 		Context used to create Views
+	 * @param pFlavor
+	 * 		Flavor that provides the markdown parsing rules
+	 * @param imageLoader
+	 * 		The ImageLoader that should be used to load images
+	 * @return MarkMark instance that renders MarkyMarkAndroid Views from Markdown
+	 */
+	public static MarkyMark<View> getMarkyMark(final Context pContext, final Flavor pFlavor, @NonNull final ImageLoader imageLoader) {
+		return getMarkyMark(pContext, pFlavor, null, null, imageLoader);
+	}
 
-    /**
-     * Creates a MarkyMark instance that converts the rules provided by the flavor into MarkyMarkAndroid Views
-     *
-     * @param pContext            Context used to create Views
-     * @param pFlavor             Flavor that provides the markdown parsing rules
-     * @param pDisplayItems       Extra DisplayItems (replaces DisplayItems that render the same MarkdownItem}
-     * @param pInlineDisplayItems Extra InlineDisplayItems (replaces InlineDisplayItems that render the same MarkdownItem}
-     * @param imageLoader         The ImageLoader that should be used to load images
-     * @return MarkMark instance that renders MarkyMarkAndroid Views from Markdown
-     */
-    public static MarkyMark<View> getMarkyMark(final Context pContext, final Flavor pFlavor,
-                                               @Nullable final List<DisplayItem> pDisplayItems, @Nullable final List<InlineDisplayItem> pInlineDisplayItems,
-                                               @NonNull ImageLoader imageLoader) {
+	/**
+	 * Creates a MarkyMark instance that converts the rules provided by the flavor into MarkyMarkAndroid Views
+	 *
+	 * @param pContext
+	 * 		Context used to create Views
+	 * @param pFlavor
+	 * 		Flavor that provides the markdown parsing rules
+	 * @param pDisplayItems
+	 * 		Extra DisplayItems (replaces DisplayItems that render the same MarkdownItem}
+	 * @param pInlineDisplayItems
+	 * 		Extra InlineDisplayItems (replaces InlineDisplayItems that render the same MarkdownItem}
+	 * @param imageLoader
+	 * 		The ImageLoader that should be used to load images
+	 * @return MarkMark instance that renders MarkyMarkAndroid Views from Markdown
+	 */
+	public static MarkyMark<View> getMarkyMark(final Context pContext, final Flavor pFlavor,
+			@Nullable final List<DisplayItem> pDisplayItems, @Nullable final List<InlineDisplayItem> pInlineDisplayItems,
+			@NonNull ImageLoader imageLoader) {
 
-        Converter<View> viewConverter = new Converter<>();
-        InlineConverter<Spanned> inlineConverter = new InlineConverter<>();
-        inlineConverter.addMapping(new BoldInlineDisplayItem());
-        inlineConverter.addMapping(new StringInlineDisplayItem());
-        inlineConverter.addMapping(new TextDisplayItem());
-        inlineConverter.addMapping(new StrikeInlineDisplayItem());
-        inlineConverter.addMapping(new ItalicInlineDisplayItem());
-        inlineConverter.addMapping(new LinkInlineDisplayItem());
-        inlineConverter.addMapping(new CodeInlineDisplayItem());
+		Converter<View> viewConverter = new Converter<>();
+		InlineConverter<Spanned> inlineConverter = new InlineConverter<>();
+		inlineConverter.addMapping(new BoldInlineDisplayItem());
+		inlineConverter.addMapping(new StringInlineDisplayItem());
+		inlineConverter.addMapping(new TextDisplayItem());
+		inlineConverter.addMapping(new StrikeInlineDisplayItem());
+		inlineConverter.addMapping(new ItalicInlineDisplayItem());
+		inlineConverter.addMapping(new LinkInlineDisplayItem());
+		inlineConverter.addMapping(new CodeInlineDisplayItem());
 
-        final ThemedContext context = new ThemedContext(pContext);
+		final ThemedContext context = new ThemedContext(pContext);
 
-        viewConverter.addMapping(new HeaderDisplayItem(context));
-        viewConverter.addMapping(new ParagraphDisplayItem(context));
-        viewConverter.addMapping(new HorizontalRuleDisplayItem(context));
-        viewConverter.addMapping(new ListDisplayItem(
-                context,
-                R.dimen.list_indentation_spacing,
-                R.dimen.list_indicator_spacing,
-                new int[]{
-                        R.drawable.bullet_filled,
-                        R.drawable.bullet,
-                        R.drawable.dash,
-                        R.drawable.square
-                })
-        );
-        viewConverter.addMapping(new QuoteDisplayItem(context));
-        viewConverter.addMapping(new CodeBlockDisplayItem(context));
-        viewConverter.addMapping(new ImageDisplayItem(context, imageLoader));
+		viewConverter.addMapping(new HeaderDisplayItem(context));
+		viewConverter.addMapping(new ParagraphDisplayItem(context));
+		viewConverter.addMapping(new HorizontalRuleDisplayItem(context));
+		viewConverter.addMapping(new ListDisplayItem(
+				context,
+				R.dimen.list_margin,
+				new int[] {
+						R.drawable.bullet_filled,
+						R.drawable.bullet,
+						R.drawable.dash,
+						R.drawable.square
+				})
+		);
+		viewConverter.addMapping(new QuoteDisplayItem(context));
+		viewConverter.addMapping(new CodeBlockDisplayItem(context));
+		viewConverter.addMapping(new ImageDisplayItem(context, imageLoader));
 
-        // Add or replace extra inline display items
-        if (pInlineDisplayItems != null) {
-            for (InlineDisplayItem item : pInlineDisplayItems) {
-                inlineConverter.addMapping(item);
-            }
-        }
+		// Add or replace extra inline display items
+		if (pInlineDisplayItems != null) {
+			for (InlineDisplayItem item : pInlineDisplayItems) {
+				inlineConverter.addMapping(item);
+			}
+		}
 
-        // Add or replace extra display items
-        if (pDisplayItems != null) {
-            for (DisplayItem item : pDisplayItems) {
-                viewConverter.addMapping(item);
-            }
-        }
+		// Add or replace extra display items
+		if (pDisplayItems != null) {
+			for (DisplayItem item : pDisplayItems) {
+				viewConverter.addMapping(item);
+			}
+		}
 
-        return new MarkyMark.Builder<View>()
-                .addFlavor(pFlavor)
-                .setConverter(viewConverter)
-                .setInlineConverter(inlineConverter)
-                .build();
-    }
+		return new MarkyMark.Builder<View>()
+				.addFlavor(pFlavor)
+				.setConverter(viewConverter)
+				.setInlineConverter(inlineConverter)
+				.build();
+	}
 }
